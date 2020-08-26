9bea32f8760237427ccbdeba068bafd884027639
Hilda Amponsah <hilda.amp@hotmail.com>
2020-08-26 15:08:37 +0100

    PostController

--- Acebook/app/controllers/posts_controller.rb
+++ Acebook/app/controllers/posts_controller.rb
@@ -1,26 +1,10 @@
 class PostsController < ApplicationController
 
-  def index
-    @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def new
   end
 
   def create
-    @post = Post.new(post_params)
-
-    @post.save
-    redirect_to @post
+    render plain: params[:post].inspect
   end
 
-  private
-
-  def post_params
-    params.require(:post).permit(:text)
-  end
 end
