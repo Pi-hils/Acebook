1260843944b0210aab0ec268937a41736a612c82
Alexis Simonian <alexis792.as@gmail.com>
2020-08-27 17:08:14 +0200

    done destroy and edit

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -21,4 +21,36 @@ feature 'Posts' do
     click_button 'Create Post'
     expect(page).to have_content('error')
   end
+
+  scenario 'posts can be edited' do
+    visit 'posts/new'
+    fill_in 'Text', with: 'Bonjour'
+    click_button 'Create Post'
+    visit '/posts'
+    click_link 'Edit'
+    expect(page).to have_content('Edit Post')
+  end
+
+  scenario 'edited posts are saved' do
+    visit 'posts/new'
+    fill_in 'Text', with: 'Bonjour'
+    click_button 'Create Post'
+    visit '/posts'
+    click_link 'Edit'
+    fill_in 'Text', with: 'The test changes things'
+    click_button 'Update Post'
+    expect(page).to have_content('The test changes things')
+    expect(page).not_to have_content('Bonjour')
+  end
+
+  scenario 'posts can be destroyed' do
+    visit 'posts/new'
+    fill_in 'Text', with: 'Bonjour'
+    click_button 'Create Post'
+    visit '/posts'
+    expect(page).to have_link('Destroy')
+    expect(page).to have_content('Bonjour')
+    click_link 'Destroy'
+    expect(page).not_to have_content('Bonjour')
+  end
 end
