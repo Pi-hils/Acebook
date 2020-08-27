1c85c3453f90cc5babe17d3572a9d3c9619a4aa0
Hilda Amponsah <hilda.amp@hotmail.com>
2020-08-27 11:48:51 +0100

    throwing Error

---
+++ spec/features/creating_posts_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper.rb'
+
+feature 'Posts' do
+
+  scenario 'list of posts' do
+    visit '/posts'
+    expect(page).to have_content('Posts thus far')
+  end
+
+  scenario 'creating a new post' do
+    visit 'posts/new'
+    fill_in 'Text', with: 'Bonjour'
+    click_button 'Create Post'
+    expect(page).to have_content('Bonjour')
+    expect(page).not_to have_content('error')
+  end
+
+  scenario 'error a new post' do
+    visit 'posts/new'
+    fill_in 'Text', with: 'Hiya'
+    click_button 'Create Post'
+    expect(page).to have_content('error')
+  end
+end
