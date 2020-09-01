98d42ca9605249576ea8eddfb63d8242769da170
DillonBarker <barker.dill98@gmail.com>
2020-09-01 10:44:44 +0100

    adding comments form and views, then refactoring out

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        # @comment = @post.comments.create(comment_params.merge(commenter: current_user.name))
+        redirect_to post_path(@post)
+      end
+     
+      private
+        def comment_params
+          params.require(:comment).permit(:commenter, :body)
+        end
 end
