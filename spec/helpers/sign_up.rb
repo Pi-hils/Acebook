27fa622752660301da714713a4ffa52526a4372d
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-09-02 12:04:01 +0100

    rspec fixes

--- spec/helpers/sign_up.rb
+++ spec/helpers/sign_up.rb
@@ -1,5 +1,6 @@
 def sign_up
   visit '/accounts/sign_up'
+  fill_in :account_username, with: 'testuser'
   fill_in :account_email, with: 'test@test.com'
   fill_in :account_password, with: '123456'
   fill_in :account_password_confirmation, with: '123456'
